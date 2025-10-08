4b35197ba22fe1dbea522997274f2682490de3ad
YerayN <ny93yeray@gmail.com>
2025-10-08 19:17:30 +0200

    Fix start.sh path and use gunicorn for production

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
-set -e
+set -e  # detiene el script si algo falla
 
-pip install -r ../requirements.txt
-python manage.py migrate
+# Instalar dependencias desde el archivo local
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Aplicar migraciones y recopilar estáticos
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:$PORT
+
+# Iniciar el servidor con Gunicorn
+gunicorn recetas_backend.wsgi:application --bind 0.0.0.0:$PORT
