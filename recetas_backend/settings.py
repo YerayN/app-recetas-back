e4f9bff9da71cfe8f3fdc3238d86030c1aeb55e4
YerayN <ny93yeray@gmail.com>
2025-10-09 09:34:55 +0200

    Arreglando panel de admin

--- recetas_backend/settings.py
+++ recetas_backend/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # ------------------------------------------------
 # 🖼️ ARCHIVOS ESTÁTICOS
 # ------------------------------------------------
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # ======================================
