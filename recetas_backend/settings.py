5e9c4ad4fe855bb82fac37848c65684627c5e855
YerayN <ny93yeray@gmail.com>
2025-10-10 06:04:46 +0200

    Conexión a PostgreSQL

--- recetas_backend/settings.py
+++ recetas_backend/settings.py
@@ -5,6 +5,7 @@ Configurado para despliegue en Railway + Vercel.
 
 import os
 from pathlib import Path
+import dj_database_url # type: ignore
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,10 +87,11 @@ WSGI_APPLICATION = "recetas_backend.wsgi.application"
 # 🗃️ BASE DE DATOS
 # ------------------------------------------------
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # ------------------------------------------------
